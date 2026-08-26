9ae1c1283dec2adf41eeb2f860eb86c27c84a72f
Ganesh Kusundal <Ganesh-Kusundal@users.noreply.github.com>
2026-08-26 18:00:04 +0530

    Revert "feat(brokers): expose .rate_limiter on ProviderHttpClient"
    This reverts commit fc091289de8c963acd6095f36da09001f921eaf8.

--- brokers/src/tradex_brokers/common/provider_client.py
+++ brokers/src/tradex_brokers/common/provider_client.py
@@ -120,7 +120,6 @@ class ProviderHttpClient:
         self._cache: ReadCache | None = (
             ReadCache(max_entries=cache_max_entries) if cache_ttl_seconds > 0 else None
         )
-        self.rate_limiter = pipeline._rate_limiter
 
     # -- helpers ------------------------------------------------------------
 
